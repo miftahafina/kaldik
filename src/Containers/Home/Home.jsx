24f72e3067e65180e8ab656805838ace1675320f
Miftah Afina <miftah.afina@gmail.com>
2020-03-02 08:15:52 +0700

    Map inside map

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,59 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import './Home.css';
 import 'normalize.css';
 import { useEffect } from 'react';
 
 
 const Home = () => {
-  const [agenda, setAgenda] = useState({
-    months: [
+  const [agenda] = useState({
+    years: [
       {
-        name: 'Januari',
-        abbr: 'Jan'
+        id: '2019',
+        name: '2019',
+        months: [
+          {
+            id: '201907',
+            name: 'Juli',
+            abbr: 'Jul'
+          },
+          {
+            id: '201908',
+            name: 'Agustus',
+            abbr: 'Ags'
+          },
+          {
+            id: '201909',
+            name: 'September',
+            abbr: 'Sep'
+          },
+          {
+            id: '201910',
+            name: 'Oktober',
+            abbr: 'Okt'
+          },
+          {
+            id: '201911',
+            name: 'November',
+            abbr: 'Nov'
+          },
+          {
+            id: '201912',
+            name: 'Desember',
+            abbr: 'Des'
+          }
+        ]
       },
       {
-        name: 'Februari',
-        abbr: 'Feb'
-      },
-      {
-        name: 'Maret',
-        abbr: 'Mar'
-      },
-      {
-        name: 'April',
-        abbr: 'Apr'
-      },
-      {
-        name: 'Mei',
-        abbr: 'Mei'
-      },
-      {
-        name: 'Juni',
-        abbr: 'Jun'
-      },
-      {
-        name: 'Juli',
-        abbr: 'Jul'
-      },
-      {
-        name: 'Agustus',
-        abbr: 'Ags'
-      },
-      {
-        name: 'September',
-        abbr: 'Sep'
-      },
-      {
-        name: 'Oktober',
-        abbr: 'Okt'
-      },
-      {
-        name: 'November',
-        abbr: 'Nov'
-      },
-      {
-        name: 'Desember',
-        abbr: 'Des'
+        id: '2020',
+        name: '2020',
+        months: [
+          {
+            id: '202001',
+            name: 'Januari',
+            abbr: 'Jan'
+          },
+          {
+            id: '202002',
+            name: 'Februari',
+            abbr: 'Feb'
+          },
+          {
+            id: '202003',
+            name: 'Maret',
+            abbr: 'Mar'
+          },
+          {
+            id: '202004',
+            name: 'April',
+            abbr: 'Apr'
+          },
+          {
+            id: '202005',
+            name: 'Mei',
+            abbr: 'Mei'
+          },
+          {
+            id: '202006',
+            name: 'Juni',
+            abbr: 'Jun'
+          }
+        ]
       }
     ]
   });
@@ -71,13 +95,23 @@ const Home = () => {
 
       <div className="content">
         <div className="months-container">
-          <div className="year">2019</div>
           {
-            agenda.months.map((data, key) => (
-              <div className="month" key={key}>
-                {data.name}
-              </div>
-            ))
+            agenda.years.map(year => {
+              return (
+                <Fragment key={year.id}>
+                  <div className="year">{year.name}</div>
+                  {
+                    year.months.map(month => {
+                      return (
+                        <div className="month" key={month.id}>
+                          {month.name}
+                        </div>
+                      )
+                    })
+                  }
+                </Fragment>
+              )
+            })
           }
         </div>
       </div>
