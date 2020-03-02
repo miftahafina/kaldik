392460ad8a76c14ac8e5ff885ea2deb42c1af195
Miftah Afina <miftah.afina@gmail.com>
2020-03-02 10:33:37 +0700

    Set index to home container

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import 'normalize.css';
 import './index.css';
 
 // Components
-import Agenda from './Containers/Agenda/Agenda'
+import Home from './Containers/Home/Home'
 
 // SW
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Agenda />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 serviceWorker.unregister();
