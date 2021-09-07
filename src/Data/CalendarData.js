284d21591cd60d81220fa65de06995ebb0bb12c8
Miftah Afina <miftah.afina@gmail.com>
2021-09-07 07:59:32 +0700

    Empty agenda

--- src/Data/CalendarData.js
+++ src/Data/CalendarData.js
@@ -1,378 +1,9 @@
 const CalendarData = {
   years: [
     {
-      id: '2020',
-      name: '2020',
-      months: [
-        {
-          id: '202006',
-          name: 'Juli',
-          abbr: 'Jul',
-          agendas: [
-            {
-              id: '20200601',
-              begin: new Date(2020, 6, 9),
-              end: null,
-              desc: 'Awal Masuk Kerja Asatidzah',
-              note: 'Tahun Pelajaran 2020/2021'
-            },
-            {
-              id: '20200602',
-              begin: new Date(2020, 6, 13),
-              end: null,
-              desc: 'Awal Masuk Santri Khidmah',
-              note: 'Tahun Pelajaran 2020/2021'
-            },
-            {
-              id: '20200603',
-              begin: new Date(2020, 6, 16),
-              end: null,
-              desc: 'Awal Kedatangan Santri Lama',
-              note: 'Dilakukan Secara Bertahap dan Terjadwal'
-            },
-            {
-              id: '20200604',
-              begin: new Date(2020, 6, 27),
-              end: null,
-              desc: 'Perkiraan Awal Kegiatan Belajar Mengajar Semester Gasal',
-              note: 'Tahun Pelajaran 2020/2021'
-            },
-            {
-              id: '20200605',
-              begin: new Date(2020, 6, 30),
-              end: null,
-              desc: 'Perkiraan Hari Arafah',
-              note: ''
-            },
-            {
-              id: '20200606',
-              begin: new Date(2020, 6, 31),
-              end: new Date(2020, 7, 1),
-              desc: 'Perkiraan Iduladha dan Kegiatan Qurban',
-              note: ''
-            },
-          ]
-        },
-        {
-          id: '202007',
-          name: 'Agustus',
-          abbr: 'Ags',
-          agendas: [
-            {
-              id: '20200701',
-              begin: new Date(2020, 6, 31),
-              end: new Date(2020, 7, 1),
-              desc: 'Perkiraan Iduladha dan Kegiatan Qurban',
-              note: ''
-            },
-            {
-              id: '20200702',
-              begin: new Date(2020, 7, 2),
-              end: null,
-              desc: 'Istirahat Asatidzah dan Santriwan/wati Pasca Iduladha dan Qurban',
-              note: 'Tidak Ada Kegiatan Belajar Mengajar'
-            },
-            {
-              id: '20200703',
-              begin: new Date(2020, 7, 17),
-              end: null,
-              desc: 'Hari Kemerdekaan Republik Indonesia 🇮🇩',
-              note: ''
-            },
-          ]
-        },
-        {
-          id: '202008',
-          name: 'September',
-          abbr: 'Sep',
-          agendas: [
-            {
-              id: '20200801',
-              begin: new Date(2020, 8, 21),
-              end: new Date(2020, 9, 1),
-              desc: 'Penilaian Tengah Semester Gasal',
-              note: ''
-            },
-          ]
-        },
-        {
-          id: '202009',
-          name: 'Oktober',
-          abbr: 'Okt',
-          agendas: [
-            {
-              id: '20200901',
-              begin: new Date(2020, 8, 21),
-              end: new Date(2020, 9, 1),
-              desc: 'Penilaian Tengah Semester Gasal',
-              note: ''
-            },
-            {
-              id: '20200902',
-              begin: new Date(2020, 9, 1),
-              end: new Date(2020, 11, 15),
-              desc: 'Pendaftaran Santriwan/wati Baru',
-              note: 'Tahun Pelajaran 2021/2022'
-            },
-          ]
-        },
-        {
-          id: '202010',
-          name: 'November',
-          abbr: 'Nov',
-          agendas: [
-            {
-              id: '20201001',
-              begin: new Date(2020, 9, 1),
-              end: new Date(2020, 11, 15),
-              desc: 'Pendaftaran Santriwan/wati Baru',
-              note: 'Tahun Pelajaran 2021/2022'
-            },
-            {
-              id: '20201002',
-              begin: new Date(2020, 10, 23),
-              end: new Date(2020, 11, 6),
-              desc: 'Penilaian Akhir Semester Gasal',
-              note: ''
-            },
-          ]
-        },
-        {
-          id: '202011',
-          name: 'Desember',
-          abbr: 'Des',
-          agendas: [
-            {
-              id: '20201101',
-              begin: new Date(2020, 9, 1),
-              end: new Date(2020, 11, 15),
-              desc: 'Pendaftaran santriwan/wati Baru',
-              note: 'Tahun Pelajaran 2021/2022'
-            },
-            {
-              id: '20201102',
-              begin: new Date(2020, 10, 23),
-              end: new Date(2020, 11, 6),
-              desc: 'Penilaian Akhir Semester Gasal',
-              note: ''
-            },
-            {
-              id: '20201103',
-              begin: new Date(2020, 11, 19),
-              end: null,
-              desc: 'Pembagian Rapor Semester Gasal',
-              note: ''
-            },
-            {
-              id: '20201104',
-              begin: new Date(2020, 11, 20),
-              end: new Date(2021, 0, 6),
-              desc: 'Libur Semester Gasal',
-              note: 'Bagi Santriwan/wati'
-            },
-            {
-              id: '20201105',
-              begin: new Date(2020, 11, 20),
-              end: new Date(2020, 11, 30),
-              desc: 'Libur Semester Gasal',
-              note: 'Bagi Asatidzah'
-            },
-            {
-              id: '20201106',
-              begin: new Date(2020, 11, 31),
-              end: new Date(2021, 0, 1),
-              desc: 'Masuk Kerja Asatidzah Persiapan PSB',
-              note: 'Hari Jum\'at Tetap Masuk'
-            },
-          ]
-        }
-      ]
-    },
-    {
       id: '2021',
       name: '2021',
       months: [
-        {
-          id: '202100',
-          name: 'Januari',
-          abbr: 'Jan',
-          agendas: [
-            {
-              id: '20210001',
-              begin: new Date(2020, 11, 20),
-              end: new Date(2021, 0, 6),
-              desc: 'Libur Semester Gasal',
-              note: 'Bagi Santriwan/wati'
-            },
-            {
-              id: '20210002',
-              begin: new Date(2020, 11, 31),
-              end: new Date(2021, 0, 1),
-              desc: 'Masuk Kerja Asatidzah Persiapan PSB',
-              note: 'Hari jum\'at Tetap Masuk'
-            },
-            {
-              id: '20210003',
-              begin: new Date(2021, 0, 2),
-              end: new Date(2021, 0, 3),
-              desc: 'Perkiraan Ujian Seleksi Calon Santriwan/wati Baru',
-              note: 'Tahun Pelajaran 2021/2022'
-            },
-            {
-              id: '20210004',
-              begin: new Date(2021, 0, 4),
-              end: new Date(2021, 0, 5),
-              desc: 'Libur Lanjutan Asatidzah dan Istirahat Pasca PSB',
-              note: ''
-            },
-            {
-              id: '20210005',
-              begin: new Date(2021, 0, 6),
-              end: null,
-              desc: 'Awal Masuk Kerja Asatidzah Semester Genap',
-              note: ''
-            },
-            {
-              id: '20210006',
-              begin: new Date(2021, 0, 7),
-              end: null,
-              desc: 'Kedatangan Santriwan/wati Masuk Semester Genap',
-              note: ''
-            },
-            {
-              id: '20210007',
-              begin: new Date(2021, 0, 9),
-              end: null,
-              desc: 'Awal Kegiatan Belajar Mengajar Semester Genap',
-              note: ''
-            },
-          ]
-        },
-        {
-          id: '202101',
-          name: 'Februari',
-          abbr: 'Feb',
-          agendas: []
-        },
-        {
-          id: '202102',
-          name: 'Maret',
-          abbr: 'Mar',
-          agendas: [
-            {
-              id: '20210201',
-              begin: new Date(2021, 2, 8),
-              end: new Date(2021, 2, 18),
-              desc: 'Penilaian Tengah Semester Genap',
-              note: ''
-            },
-          ]
-        },
-        {
-          id: '202103',
-          name: 'April',
-          abbr: 'Apr',
-          agendas: [
-            {
-              id: '20210301',
-              begin: new Date(2021, 3, 1),
-              end: new Date(2021, 3, 20),
-              desc: 'Perkiraan Ujian Madrasah Jenjang MA',
-              note: ''
-            },
-            {
-              id: '20210302',
-              begin: new Date(2021, 3, 13),
-              end: null,
-              desc: 'Perkiraan Awal Ramadhan 1442 H',
-              note: ''
-            },
-            {
-              id: '20210303',
-              begin: new Date(2021, 3, 19),
-              end: new Date(2021, 3, 30),
-              desc: 'Perkiraan Ujian Madrasah Jenjang MTs',
-              note: ''
-            },
-          ]
-        },
-        {
-          id: '202104',
-          name: 'Mei',
-          abbr: 'Mei',
-          agendas: [
-            {
-              id: '20210401',
-              begin: new Date(2021, 4, 1),
-              end: null,
-              desc: 'Kepulangan Santriwan/wati Untuk Liburan Ramadhan dan Idulfitri',
-              note: ''
-            },
-            {
-              id: '20210402',
-              begin: new Date(2021, 4, 2),
-              end: new Date(2021, 4, 22),
-              desc: 'Libur Ramadhan dan Idulfitri',
-              note: 'Bagi santriwan/wati'
-            },
-            {
-              id: '20210403',
-              begin: new Date(2021, 4, 2),
-              end: new Date(2021, 4, 21),
-              desc: 'Libur Ramadhan dan Idulfitri',
-              note: 'Bagi asatidzah'
-            },
-            {
-              id: '20210404',
-              begin: new Date(2021, 4, 22),
-              end: null,
-              desc: 'Awal Masuk Kerja Asatidzah Pasca Libur Idulfitri',
-              note: ''
-            },
-            {
-              id: '20210405',
-              begin: new Date(2021, 4, 23),
-              end: null,
-              desc: 'Kedatangan Santriwan/wati Pasca Libur Idulfitri',
-              note: 'Maksimal Pukul 15.00 WIB'
-            },
-          ]
-        },
-        {
-          id: '202105',
-          name: 'Juni',
-          abbr: 'Jun',
-          agendas: [
-            {
-              id: '20210501',
-              begin: new Date(2021, 5, 2),
-              end: new Date(2021, 5, 15),
-              desc: 'Ulangan Kenaikan Kelas (UKK) / Penilaian Akhir Tahun',
-              note: ''
-            },
-            {
-              id: '20210503',
-              begin: new Date(2021, 5, 26),
-              end: null,
-              desc: 'Pembagian Rapor Semester Genap',
-              note: ''
-            },
-            {
-              id: '20210504',
-              begin: new Date(2021, 5, 27),
-              end: new Date(2021, 6, 16),
-              desc: 'Libur Kenaikan Kelas',
-              note: 'Bagi Santriwan/wati'
-            },
-            {
-              id: '20210505',
-              begin: new Date(2021, 5, 27),
-              end: new Date(2021, 6, 11),
-              desc: 'Libur Kenaikan Kelas',
-              note: 'Bagi Asatidzah'
-            },
-          ]
-        },
         {
           id: '202106',
           name: 'Juli',
@@ -380,66 +11,176 @@ const CalendarData = {
           agendas: [
             {
               id: '20210601',
-              begin: new Date(2021, 5, 27),
-              end: new Date(2021, 6, 16),
-              desc: 'Libur Kenaikan Kelas',
-              note: 'Bagi Santriwan/wati'
-            },
-            {
-              id: '20210602',
-              begin: new Date(2021, 5, 27),
-              end: new Date(2021, 6, 11),
-              desc: 'Libur Kenaikan Kelas',
-              note: 'Bagi Asatidzah'
-            },
-            {
-              id: '20210603',
-              begin: new Date(2021, 6, 12),
+              begin: new Date(2021, 6, 9),
               end: null,
-              desc: 'Awal Masuk Kerja Asatidzah TP. 2021/2022',
-              note: ''
+              desc: '',
+              note: '',
             },
+          ]
+        },
+        {
+          id: '202107',
+          name: 'Agustus',
+          abbr: 'Ags',
+          agendas: [
             {
-              id: '20210604',
-              begin: new Date(2021, 6, 17),
+              id: '20210701',
+              begin: new Date(2021, 6, 31),
+              end: new Date(2021, 7, 1),
+              desc: '',
+              note: '',
+            },
+          ]
+        },
+        {
+          id: '202108',
+          name: 'September',
+          abbr: 'Sep',
+          agendas: [
+            {
+              id: '20210801',
+              begin: new Date(2021, 8, 21),
+              end: new Date(2021, 9, 1),
+              desc: '',
+              note: '',
+            },
+          ]
+        },
+        {
+          id: '202109',
+          name: 'Oktober',
+          abbr: 'Okt',
+          agendas: [
+            {
+              id: '20210901',
+              begin: new Date(2021, 8, 21),
+              end: new Date(2021, 9, 1),
+              desc: '',
+              note: '',
+            },
+          ]
+        },
+        {
+          id: '202110',
+          name: 'November',
+          abbr: 'Nov',
+          agendas: [
+            {
+              id: '20211001',
+              begin: new Date(2021, 9, 1),
+              end: new Date(2021, 11, 15),
+              desc: '',
+              note: '',
+            },
+          ]
+        },
+        {
+          id: '202111',
+          name: 'Desember',
+          abbr: 'Des',
+          agendas: [
+            {
+              id: '20211101',
+              begin: new Date(2021, 9, 1),
+              end: new Date(2021, 11, 15),
+              desc: '',
+              note: '',
+            },
+          ]
+        }
+      ]
+    },
+    {
+      id: '2022',
+      name: '2022',
+      months: [
+        {
+          id: '202200',
+          name: 'Januari',
+          abbr: 'Jan',
+          agendas: [
+            {
+              id: '20220001',
+              begin: new Date(2021, 11, 20),
+              end: new Date(2022, 0, 6),
+              desc: '',
+              note: '',
+            },
+          ]
+        },
+        {
+          id: '202201',
+          name: 'Februari',
+          abbr: 'Feb',
+          agendas: []
+        },
+        {
+          id: '202202',
+          name: 'Maret',
+          abbr: 'Mar',
+          agendas: [
+            {
+              id: '20220211',
+              begin: new Date(2022, 2, 8),
+              end: new Date(2022, 2, 18),
+              desc: '',
+              note: '',
+            },
+          ]
+        },
+        {
+          id: '202203',
+          name: 'April',
+          abbr: 'Apr',
+          agendas: [
+            {
+              id: '20220301',
+              begin: new Date(2022, 3, 1),
+              end: new Date(2022, 3, 20),
+              desc: '',
+              note: '',
+            },
+          ]
+        },
+        {
+          id: '202204',
+          name: 'Mei',
+          abbr: 'Mei',
+          agendas: [
+            {
+              id: '20220401',
+              begin: new Date(2022, 4, 1),
               end: null,
-              desc: 'Awal Masuk Santriwan/wati Baru Diantar Orang Tua/wali (PWSB)',
-              note: 'Pukul 7.00 WIB'
+              desc: '',
+              note: '',
             },
+          ]
+        },
+        {
+          id: '202205',
+          name: 'Juni',
+          abbr: 'Jun',
+          agendas: [
             {
-              id: '20210605',
-              begin: new Date(2021, 6, 18),
-              end: null,
-              desc: 'Kedatangan Santriwan/wati Lama',
-              note: 'Maksimal pukul 16.00 WIB'
+              id: '20220501',
+              begin: new Date(2022, 5, 2),
+              end: new Date(2022, 5, 15),
+              desc: '',
+              note: '',
             },
+          ]
+        },
+        {
+          id: '202206',
+          name: 'Juli',
+          abbr: 'Jul',
+          agendas: [
             {
-              id: '20210606',
-              begin: new Date(2021, 6, 20),
-              end: new Date(2021, 6, 22),
-              desc: 'Perkiraan Iduladha dan Kegiatan Qurban',
-              note: ''
-            },
-            {
-              id: '20210607',
-              begin: new Date(2021, 6, 24),
-              end: new Date(2021, 6, 26),
-              desc: 'Masa Orientasi Santri Baru',
-              note: ''
-            },
-            {
-              id: '20210608',
-              begin: new Date(2021, 6, 24),
-              end: null,
-              desc: 'Awal Kegiatan Belajar Mengajar Semester Gasal TP. 2021/2022',
-              note: 'Bagi Santriwan/wati Lama'
-            },
-            {
-              id: '20210609',
-              begin: new Date(2021, 6, 27),
-              end: null,
-              desc: 'Awal Kegiatan Belajar Mengajar Semester Gasal TP. 2021/2022',
-              note: 'Bagi Santriwan/wati Baru'
+              id: '20220601',
+              begin: new Date(2022, 5, 27),
+              end: new Date(2022, 6, 16),
+              desc: '',
+              note: '',
             },
           ]
         }
